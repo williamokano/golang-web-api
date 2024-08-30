8966dd7929f8ec042e230af788abdefdf313781a
William Okano <williamokano@gmail.com>
2024-08-30 19:19:54 +0200

    feat: added basic server and graceful shutdown support

--- main.go
+++ main.go
@@ -1,8 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Hello World")
-}
+	router := gin.Default()
 
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router.Handler(),
+	}
+
+	go func() {
+		// start listening to connections
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// wait for interrupt signal to gracefully shutdown the server
+	// with a timeout of 5 seconds
+	quit := make(chan os.Signal, 1)
+
+	// add listener for sigint and sigterm as sigkill cannot be catch
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	// catch ctx.Done() with 5 seconds timeout
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
+}
